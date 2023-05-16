79ff12750065a1a673241eace19f7f41bdd30bcd
EC-K <elxone.19jt@gmail.com>
2023-05-16 14:21:30 +0900

    chore: Change first loaded avatar

--- src/stores/VRMAvatar.ts
+++ src/stores/VRMAvatar.ts
@@ -52,5 +52,5 @@ class VRMAvatar {
 }
 
 export const vrmAvatar = new VRMAvatar(
-  'https://cdn.glitch.com/29e07830-2317-4b15-a044-135e73c7f840%2FAshtra.vrm?v=1630342336981',
+  './first_loaded_avatar.vrm',
 )
