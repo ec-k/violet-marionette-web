79a6d08ef9a2288e4fce5f42ad406081365a04e5
EC-K <elxone.19jt@gmail.com>
2023-11-29 13:35:06 +0900

    feat: Remove vrm's spring bones.
    If spring bones are present, they can get caught somewhere on the body and prevent proper movement when arms move.

--- src/models/vrm-toy-box-ik-solver/Avatar.ts
+++ src/models/vrm-toy-box-ik-solver/Avatar.ts
@@ -8,8 +8,8 @@ import { trackingSettings } from 'stores/settings'
 
 export class Avatar {
   private _scene: THREE.Scene | null = null
-  public vrm: VRM | null
-  public avatarSrc: string | null = null
+  vrm: VRM | null
+  avatarSrc: string | null = null
   private _vrmIK: VrmIK | null = null
   private _vrmFK: VrmFK | null = null
 
@@ -43,7 +43,7 @@ export class Avatar {
   }
 
   // VRMの読み込み
-  public async loadVRM(url: string) {
+  async loadVRM(url: string) {
     if (!this._scene) return
     if (this.vrm) {
       this._scene.remove(this.vrm.scene)
@@ -58,9 +58,18 @@ export class Avatar {
 
     this._vrmIK = new VrmIK(vrm)
     this._vrmFK = new VrmFK()
+    this._removeSpringBone(vrm)
   }
 
-  public updatePose() {
+  private _removeSpringBone(vrm: VRM) {
+    const list = vrm.springBoneManager?.springBoneGroupList
+    if (!list) return
+    while (list.length > 0) {
+      list.pop()
+    }
+  }
+
+  updatePose() {
     const enabledIK = trackingSettings.enabledIK
     if (enabledIK && !!this._vrmIK) this._vrmIK.solve()
     if (!!this._vrmFK && !!this.vrm) this._vrmFK.setPose(this.vrm, !enabledIK)
