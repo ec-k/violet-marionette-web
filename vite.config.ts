4684d344e6c070993d0d751467d020ed87376323
EC-K <elxone.19jt@gmail.com>
2025-09-01 12:22:54 +0900

    chore: change build settings

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import path from 'path'
+import license from 'rollup-plugin-license'
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -10,5 +12,21 @@ export default defineConfig({
       jsxImportSource: '@emotion/react',
     }),
     tsconfigPaths(),
+    license({
+      sourcemap: true,
+      thirdParty: {
+        includePrivate: true,
+        multipleVersions: true,
+        output: {
+          file: path.join(__dirname, 'dist', 'license.txt'),
+          encoding: 'utf-8',
+        },
+      },
+    }),
   ],
+  resolve: {
+    alias: {
+      '@': '/src',
+    },
+  },
 })
