7ac6dc7a86253e95fba1b8c3a13f963b5cf28e57
EC-K <elxone.19jt@gmail.com>
2025-09-03 09:25:35 +0900

    fix: fix case of blendshape names

--- src/models/avatar/faceSolver.ts
+++ src/models/avatar/faceSolver.ts
@@ -1,5 +1,6 @@
 import type { FaceLandmarkerResult } from '@mediapipe/tasks-vision'
 import type { VRM } from '@pixiv/three-vrm'
+import { camelToPascal } from '../utils'
 
 class FaceSolver {
   applyPerfectSyncResult(vrm: VRM, result: FaceLandmarkerResult) {
@@ -7,8 +8,9 @@ class FaceSolver {
     const manager = vrm.expressionManager
     const blendshapes = result.faceBlendshapes
     blendshapes[0].categories.forEach((blendshape) => {
-      const key = blendshape.categoryName
+      const key = camelToPascal(blendshape.categoryName)
       const weight = blendshape.score
+      console.log(`${key}, ${weight}`)
       manager?.setValue(key, weight)
     })
   }
